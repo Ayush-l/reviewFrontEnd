f1e2732d208e353b5d95c25d11975dcd840aae26
Ayush-l <aayushlingwal24@gmail.com>
2025-12-27 18:57:48 +0530

    CHANGED

--- src/Cards/Header.jsx
+++ src/Cards/Header.jsx
@@ -1,20 +1,28 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "../CSS/Header.css";
 
 const Header = () => {
+  const navigate = useNavigate();
+
   const handleLogout = () => {
-    alert("Logged out!");
     localStorage.removeItem("jwtTokenPauriWebSite");
     localStorage.removeItem("role");
     navigate("/login");
   };
+
   return (
     <header className="site-header">
-      <div className="header-content">
-        <h1>Restaurant / Cafe Review Website — Pauri</h1>
-        <p>Discover • Review • Share your food experience</p>
+      <div className="header-inner">
+        <div className="header-content">
+          <h1>Restaurant / Cafe Review Website — Pauri</h1>
+          <p>Discover • Review • Share your food experience</p>
+        </div>
+
+        <button className="logout-btn" onClick={handleLogout}>
+          Logout
+        </button>
       </div>
-      <button className="logout-btn" onClick={handleLogout}>Logout</button>
     </header>
   );
 };
