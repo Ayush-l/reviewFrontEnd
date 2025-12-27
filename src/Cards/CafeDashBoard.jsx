7906ce6a9f04999f60a8ba602444107a183df060
Ayush-l <aayushlingwal24@gmail.com>
2025-12-27 17:11:58 +0530

    CHANGES

--- src/Cards/CafeDashBoard.jsx
+++ src/Cards/CafeDashBoard.jsx
@@ -98,7 +98,7 @@ const CafeDashboard = () => {
               <img src={dish.url} alt={dish.name} />
               <h4>{dish.name}</h4>
               <StarRating rating={dish.rating || 0} />
-              <p>{dish.ratingCount[idx][0]+dish.ratingCount[idx][1]+dish.ratingCount[idx][2]+dish.ratingCount[idx][3]+dish.ratingCount[idx][4]} reviews</p>
+              <p>{dish.ratingCount?dish.ratingCount[idx][0]+dish.ratingCount[idx][1]+dish.ratingCount[idx][2]+dish.ratingCount[idx][3]+dish.ratingCount[idx][4]:0} reviews</p>
               <button
                 className="reviews-btn"
                 onClick={() => navigate(`/dishreviews/${cafe.id}/${dish.name}`)}
